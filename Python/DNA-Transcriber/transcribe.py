feaec03031887dcfdcbdff04c227f49cad3a3e3f
Alexandre <alexhamiladeveloper@gmail.com>
2020-10-08 20:20:45 +0200

    Add files via upload

---
+++ Python/DNA-Transcriber/transcribe.py
@@ -0,0 +1,17 @@
+def main():
+    '''Asks for a dna sequence and transcribe it.'''
+    dna_sequence = input("Please, enter a DNA sequence: ")
+    rna_sequence = ""
+    valid_nucleotide = set("ACGT")
+
+    for nucleotide in dna_sequence:
+        if nucleotide not in valid_nucleotide:
+            print(nucleotide, " is not a valid nucleotide.")
+            sys.exit()
+            if nucleotide == "T":
+                rna_sequence = rna_sequence + "U"
+            else:
+                rna_sequence = rna_sequence + nucleotide
+    print(rna_sequence)
+
+main()
